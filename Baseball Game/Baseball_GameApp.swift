c7f6b11abc0617bbaedb4498508b1a2f37ad2f4d
ZiXiang <wzxiang9301@gmail.com>
2024-11-24 17:15:51 +0800

    Initial Commit

---
+++ Baseball Game/Baseball_GameApp.swift
@@ -0,0 +1,17 @@
+//
+//  Baseball_GameApp.swift
+//  Baseball Game
+//
+//  Created by 翁子翔 on 2024/11/24.
+//
+
+import SwiftUI
+
+@main
+struct Baseball_GameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Baseball GameTests/Baseball_GameTests.swift
@@ -0,0 +1,17 @@
+//
+//  Baseball_GameTests.swift
+//  Baseball GameTests
+//
+//  Created by 翁子翔 on 2024/11/24.
+//
+
+import Testing
+@testable import Baseball_Game
+
+struct Baseball_GameTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
